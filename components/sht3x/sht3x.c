1597f146244dd888538c8392c930758352bf1356
netbolo <bolomis@gmail.com>
2019-05-20 14:50:36 +0800

    git push origin master

--- components/sht3x/sht3x.c
+++ components/sht3x/sht3x.c
@@ -1,45 +1,7 @@
 /*
  * Driver for Sensirion SHT3x digital temperature and humidity sensor
  * connected to I2C
- *
- * This driver is for the usage with the ESP8266 and FreeRTOS (esp-open-rtos)
- * [https://github.com/SuperHouse/esp-open-rtos]. It is also working with ESP32
- * and ESP-IDF [https://github.com/espressif/esp-idf.git] as well as Linux
- * based systems using a wrapper library for ESP8266 functions.
- *
- * ----------------------------------------------------------------
- *
- * The BSD License (3-clause license)
- *
- * Copyright (c) 2017 Gunar Schorcht (https://github.com/gschorcht)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its 
- * contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+*/
 
 #include "sht3x.h"
 
@@ -171,36 +133,9 @@ esp_err_t I2C_SHT3x_Read(uint8_t reg_address, uint8_t *data, size_t data_len)
 
     return ret;
 }
-esp_err_t I2C_SHT3x_Read2BytesAndCrc(uint16_t *data1, uint16_t *data2)
+esp_err_t I2C_SHT3x_Read6BytesAndCrc(uint16_t *rawValueTemperature, uint16_t *rawValueHumidity)
 {
-    int ret;
-    uint8_t bytes[6];
-    uint8_t crcdata1[2], crcdata2[2];
-    uint8_t checksum1, checksum2;
-
-    ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_MEAS_POLLING_H));
-    ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_FETCH_DATA));
-    ESP_ERROR_CHECK(I2C_SHT3x_Read(0, bytes, 6));
-    ESP_LOGI(TAG, "bytes[0]= 0x%0x, bytes[1]= 0x%0x, bytes[2]= 0x%0x\n", bytes[0], bytes[1], bytes[2]);
-    ESP_LOGI(TAG, "bytes[3]= 0x%0x, bytes[4]= 0x%0x, bytes[5]= 0x%0x\n", bytes[3], bytes[4], bytes[5]);
-    
-    memcpy(crcdata1, bytes, 2);
-    checksum1 = bytes[2];
-    ESP_LOGI(TAG, "crcdata1[0]= 0x%0x, crcdata1[1]= 0x%0x, checksum1= 0x%0x\n", crcdata1[0], crcdata1[1], checksum1);
-    ret = SHT3x_CheckCrc(crcdata1, 2, checksum1);
-    *data1 = (crcdata1[0] << 8) | crcdata1[1];
-
-    memcpy(crcdata2, bytes+3, 2);
-    checksum2 = bytes[5];
-    ESP_LOGI(TAG, "crcdata2[0]= 0x%0x, crcdata2[1]= 0x%0x, checksum2= 0x%0x\n", crcdata2[0], crcdata2[1], checksum2);
-    ret = SHT3x_CheckCrc(crcdata2, 2, checksum2);
-    *data2 = (crcdata2[0] << 8) | crcdata2[1];
-
-    return ret;
-}
-esp_err_t I2C_SHT3x_ReadMeasurementBuffer(float *temperature, float *humidity)
-{
-    int ret;
+    esp_err_t ret;
     uint16_t rawValueTemp;       //temperature raw value from sensor
     uint16_t rawValueHumi;       //humidity raw value from sensor
 
@@ -208,9 +143,48 @@ esp_err_t I2C_SHT3x_ReadMeasurementBuffer(float *temperature, float *humidity)
     uint8_t rawDataTemp[2], rawDataHumi[2];
     uint8_t checksumTemp, checksumHumi;
 
-    ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_MEAS_POLLING_H));    
-    vTaskDelay(100 / portTICK_RATE_MS);
     ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_FETCH_DATA));
+    vTaskDelay(100 / portTICK_RATE_MS);
+    ESP_ERROR_CHECK(I2C_SHT3x_Read(0, rawData, 6));
+
+    ESP_LOGI(TAG, "rawData[0]= 0x%0x, rawData[1]= 0x%0x, rawData[2]= 0x%0x\n", rawData[0], rawData[1], rawData[2]);
+    ESP_LOGI(TAG, "rawData[3]= 0x%0x, rawData[4]= 0x%0x, rawData[5]= 0x%0x\n", rawData[3], rawData[4], rawData[5]);
+    
+    memcpy(rawDataTemp, rawData, 2);
+    checksumTemp = rawData[2];
+    ESP_LOGI(TAG, "rawDataTemp[0]= 0x%0x, rawDataTemp[1]= 0x%0x, checksumTemp= 0x%0x\n", rawDataTemp[0], rawDataTemp[1], checksumTemp);
+    ret = SHT3x_CheckCrc(rawDataTemp, 2, checksumTemp);
+    if(ret == ESP_OK)
+    {
+    *rawValueTemperature = (rawDataTemp[0] << 8) | rawDataTemp[1];
+    ESP_LOGI(TAG, "rawValueTemperature= 0x%0x\n", *rawValueTemperature);
+    }
+
+    memcpy(rawDataHumi, rawData+3, 2);
+    checksumHumi = rawData[5];
+    ESP_LOGI(TAG, "rawDataHumi[0]= 0x%0x, rawDataHumi[1]= 0x%0x, checksumHumi= 0x%0x\n", rawDataHumi[0], rawDataHumi[1], checksumHumi);
+    ret = SHT3x_CheckCrc(rawDataHumi, 2, checksumHumi);
+    if(ret == ESP_OK)
+    {
+    *rawValueHumidity = (rawDataHumi[0] << 8) | rawDataHumi[1];
+    ESP_LOGI(TAG, "rawValueHumidity= 0x%0x\n", *rawValueHumidity);
+    }
+
+    return ret;
+}
+esp_err_t I2C_SHT3x_ReadMeasurementBuffer(float *temperature, float *humidity)
+{
+    esp_err_t ret;
+    uint16_t rawValueTemp;       //temperature raw value from sensor
+    uint16_t rawValueHumi;       //humidity raw value from sensor
+
+    uint8_t rawData[6];
+    uint8_t rawDataTemp[2], rawDataHumi[2];
+    uint8_t checksumTemp, checksumHumi;
+
+    // ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_MEAS_POLLING_H));    
+    // vTaskDelay(100 / portTICK_RATE_MS);
+    // ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_FETCH_DATA));
     vTaskDelay(100 / portTICK_RATE_MS);
     ESP_ERROR_CHECK(I2C_SHT3x_Read(0, rawData, 6));
 
@@ -238,8 +212,123 @@ esp_err_t I2C_SHT3x_ReadMeasurementBuffer(float *temperature, float *humidity)
     ESP_LOGI(TAG, "rawValueHumi= 0x%0x\n", rawValueHumi);
     *humidity = SHT3x_CalcHumidity(rawValueHumi);
     }
-
+    
+    ESP_LOGI(TAG, "temperature= %d.%d, humidity= %d.%d\n", (uint16_t)*temperature, (uint16_t)(*temperature *100) %100,(uint16_t)*humidity,(uint16_t)(*humidity *100) %100);
     return ret;
+}
+esp_err_t SHT3x_StartPeriodicMeasurement(etRepeatability repeatability, etFrequency frequency)
+{
+    esp_err_t ret;
+
+    switch(repeatability)
+    {
+        case REPEATAB_LOW:
+        switch(frequency)
+        {
+            case FREQUENCY_HZ5:         //low repeatability 0.5Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_05_L);
+            break;
+            case FREQUENCY_1HZ:         // low repeatability 1.0Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_1_L);
+            break;
+          case FREQUENCY_2HZ:  // low repeatability,  2.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_2_L);
+            break;          
+          case FREQUENCY_4HZ:  // low repeatability,  4.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_4_L);
+            break;          
+          case FREQUENCY_10HZ: // low repeatability, 10.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_10_L);
+            break;          
+          default:
+            ret = ESP_FAIL;
+            break;
+        }
+        break;
+
+      case REPEATAB_MEDIUM: // medium repeatability
+        switch(frequency)
+        {
+          case FREQUENCY_HZ5:  // medium repeatability,  0.5 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_05_M);
+			break;
+          case FREQUENCY_1HZ:  // medium repeatability,  1.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_1_M);
+			break;        
+          case FREQUENCY_2HZ:  // medium repeatability,  2.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_2_M);
+			break;        
+          case FREQUENCY_4HZ:  // medium repeatability,  4.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_4_M);
+			break;      
+          case FREQUENCY_10HZ: // medium repeatability, 10.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_10_M);
+			break;
+          default:
+            ret = ESP_FAIL;
+			break;
+        }
+        break;
+        
+      case REPEATAB_HIGH: // high repeatability
+        switch(frequency)
+        {
+          case FREQUENCY_HZ5:  // high repeatability,  0.5 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_05_H);
+            break;
+          case FREQUENCY_1HZ:  // high repeatability,  1.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_1_H);
+            break;
+          case FREQUENCY_2HZ:  // high repeatability,  2.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_2_H);
+            break;
+          case FREQUENCY_4HZ:  // high repeatability,  4.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_4_H);
+            break;
+          case FREQUENCY_10HZ: // high repeatability, 10.0 Hz
+            ret = I2C_SHT3x_WriteCommand(CMD_MEAS_PERI_10_H);
+            break;
+          default:
+            ret = ESP_FAIL;
+            break;
+        }
+        break;
+      default:
+        ret = ESP_FAIL;
+        break;
+    }
+    return ret;
+}
+esp_err_t SHT3x_GetTempAndHumiClkStretch(float *temperature, float *humidity, etRepeatability repeatability)
+{
+    esp_err_t ret;
+    uint16_t rawValueTemp;          //temperature raw value from sensor
+    uint16_t rawValueHumi;          //humidity raw value from sensor
+
+    switch(repeatability)
+    {
+      case REPEATAB_LOW:
+        ret = I2C_SHT3x_WriteCommand(CMD_MEAS_CLOCKSTR_L);
+        break;
+      case REPEATAB_MEDIUM:
+        ret = I2C_SHT3x_WriteCommand(CMD_MEAS_CLOCKSTR_M);
+        break;
+      case REPEATAB_HIGH:
+        ret = I2C_SHT3x_WriteCommand(CMD_MEAS_CLOCKSTR_H);
+        break;
+      default:
+        ret = ESP_FAIL;
+        break;
+    }
+    if(ret == ESP_OK)
+    {
+        ret =  I2C_SHT3x_Read6BytesAndCrc(&rawValueTemp, &rawValueHumi);
+        if(ret == ESP_OK)
+        *temperature = SHT3x_CalcTemperature(rawValueTemp);
+        *humidity = SHT3x_CalcHumidity(rawValueHumi);
+        ESP_LOGI(TAG, "temperature= %d.%d, humidity= %d.%d\n", (uint16_t)*temperature, (uint16_t)(*temperature *100) %100,(uint16_t)*humidity,(uint16_t)(*humidity *100) %100);
+    }
+    return ret;    
 }
 static uint8_t SHT3x_CalcCrc(uint8_t data[], uint8_t nbrOfBytes)
 {
@@ -340,21 +429,24 @@ esp_err_t SHT3x_ReadSerialNumber(uint32_t *SerialNumber)
 void I2C_SHT3x_Task(void *arg)
 {
     // uint8_t data[3];
+    int ret;
     float temperature;
     float humidity;
     uint32_t SerialNumber;
     I2C_SHT3x_Init();
     // ESP_ERROR_CHECK(I2C_SHT3x_WriteCommand(CMD_MEAS_POLLING_H));
-    int ret;
+    ret = SHT3x_StartPeriodicMeasurement(REPEATAB_HIGH, FREQUENCY_1HZ);
     while(1)
     {
-    ret = I2C_SHT3x_ReadMeasurementBuffer(&temperature, &humidity);
+    
     // ESP_ERROR_CHECK(I2C_SHT3x_Read(I2C_SHT3X_MASTER_NUM, 0, data, 3));
     // ESP_LOGI(TAG, "data[0]= 0x%0x, data[1]= 0x%0x, data[2]= 0x%0x\n", data[0], data[1], data[2]);
     // ESP_ERROR_CHECK(SHT3x_ReadSerialNumber(&SerialNum));
     if(ret == ESP_OK)
         {
-            ESP_LOGI(TAG, "temperature= %d.%d, humidity= %d.%d\n", (uint16_t)temperature, (uint16_t)(temperature *100) %100,(uint16_t)humidity,(uint16_t)(humidity *100) %100);
+        ret = I2C_SHT3x_ReadMeasurementBuffer(&temperature, &humidity);
+            
+            //ESP_LOGI(TAG, "temperature= %d.%d, humidity= %d.%d\n", (uint16_t)temperature, (uint16_t)(temperature *100) %100,(uint16_t)humidity,(uint16_t)(humidity *100) %100);
             // SerialNumber = (SerialNumWords[0] << 16 | SerialNumWords[1]);
             // ESP_LOGI(TAG, "SerialNumber = 0x%0x\n", SerialNumber);
         }
--- components/sht3x/sht3x.h
+++ components/sht3x/sht3x.h
@@ -1,44 +1,6 @@
 /*
  * Driver for Sensirion SHT3x digital temperature and humidity sensor
  * connected to I2C
- *
- * This driver is for the usage with the ESP8266 and FreeRTOS (esp-open-rtos)
- * [https://github.com/SuperHouse/esp-open-rtos]. It is also working with ESP32
- * and ESP-IDF [https://github.com/espressif/esp-idf.git] as well as Linux
- * based systems using a wrapper library for ESP8266 functions.
- *
- * ----------------------------------------------------------------
- *
- * The BSD License (3-clause license)
- *
- * Copyright (c) 2017 Gunar Schorcht (https://github.com/gschorcht)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its 
- * contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
  */
  
 #ifndef __SHT3x_H__
@@ -159,7 +121,26 @@ typedef enum{
   CMD_W_AL_LIM_LS     = 0x6100, // write alert limits, low set
   CMD_NO_SLEEP        = 0x303E,
 }etCommands;
+// Measurement Repeatability
+typedef enum{
+  REPEATAB_HIGH,   // high repeatability
+  REPEATAB_MEDIUM, // medium repeatability
+  REPEATAB_LOW,    // low repeatability
+}etRepeatability;
 
+// Measurement Mode
+typedef enum{
+  MODE_CLKSTRETCH, // clock stretching
+  MODE_POLLING,    // polling
+}etMode;
+
+typedef enum{
+  FREQUENCY_HZ5,  //  0.5 measurements per seconds
+  FREQUENCY_1HZ,  //  1.0 measurements per seconds
+  FREQUENCY_2HZ,  //  2.0 measurements per seconds
+  FREQUENCY_4HZ,  //  4.0 measurements per seconds
+  FREQUENCY_10HZ, // 10.0 measurements per seconds
+}etFrequency;
 /**
  * @brief	raw data type
  */
